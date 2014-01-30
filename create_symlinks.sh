1f4b69c3efb1a35087f281cac95df5447c744bd7
Jan Ivar Beddari <code@beddari.net>
2014-01-30 10:20:29 +0100

    Simple script to create symlinks

---
+++ create_symlinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash   
+ln -sf ~/dotfiles/bashrc ~/.bashrc 
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig 
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf 
+mkdir -p ~/.vim/colors
+ln -sf ~/dotfiles/vim_colors_solarized.vim ~/.vim/colors/solarized.vim
+ln -sf ~/dotfiles/vimrc ~/.vimrc
