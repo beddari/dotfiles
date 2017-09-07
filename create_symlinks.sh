ef8101a4866f485dc13af7adc76ccf2d2b938985
Jan Ivar Beddari <code@beddari.net>
2017-09-07 11:10:09 +0200

    vscode settings

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,8 +1,10 @@
-#!/bin/bash   
-ln -sf ~/dotfiles/bashrc ~/.bashrc 
-ln -sf ~/dotfiles/gitconfig ~/.gitconfig 
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf 
+#!/bin/bash
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 mkdir -p ~/.vim/colors
 ln -sf ~/dotfiles/vim_colors_solarized.vim ~/.vim/colors/solarized.vim
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/dircolors.ansi-dark ~/.dir_colors
+mkdir -p ~/.config/Code/User
+ln -sf ~/dotfiles/vscode.json ~/.config/Code/User/settings.json
